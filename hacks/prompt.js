6e3fc1dd44ace3f18de4fdadfa72d7cc23298ad4
Julian Pineda <miroamarillo@gmail.com>
2016-04-08 22:43:53 -0400

    trying color on icon again

--- hacks/prompt.js
+++ hacks/prompt.js
@@ -19,6 +19,6 @@ prompt = function() {
     }
     var state = isMongos() ? '[mongos]' : rs_state;
     // return host + '(' + process + '-' + version + ')' + state + ' ' + db + '> ';
-    return host + state + ' ' + icon + ' ' + db + '> ';
+    return host + state + ' ' + colorize(icon, {color: "cyan"}) + ' ' + db + '> ';
 };
 
